e0541c6bfd4b0764204af2f99ca9076d4f497c98
VolodymyrHladii <volodymyr.hladii@beleven.dev>
2025-06-11 14:19:12 +0300

    Added unique language name

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Content(models.Model):
-    name = models.TextField()
+    name = models.TextField(unique=True)
     href = models.TextField()
     logo_alt = models.TextField()
     logo = models.TextField()
